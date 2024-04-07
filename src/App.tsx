b1ed06b463f38a0ea4af6425bb36cc557e936a1a
Carlos Daniel <carlos.daniel.0486@gmail.com>
2024-04-07 11:43:32 -0300

    instalando biblioteca de manipulção de data e hora date-fns

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,15 @@ import { Header } from "./components/Header/Header";
 import styles from "./App.module.css";
 
 import "./global.css";
+import { Sidebar } from "./components/Sidebar/Sidebar";
 
 function App() {
   return (
     <>
       <Header />
+      <div className={styles.wrapper}>
+        <Sidebar />
+      </div>
     </>
   );
 }
